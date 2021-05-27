47d3211f2982e6abed323b59522e738a33119993
Ben Norrichs <ben.norrichs@gmail.com>
2021-05-27 14:03:51 -0400

    3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, useHistory } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import AgencyProfile from "./pages/AgencyProfile";
 import Booking from "./pages/Booking";
@@ -7,77 +7,70 @@ import CustomerProfile from "./pages/CustomerProfile";
 import Home from "./pages/Home";
 import PopularTrips from "./pages/PopularTrips";
 import Trip from "./pages/Trip";
-import Login from "./pages/Login";
 
 function App() {
 	/// STATE VARIABLES ///
-	const [activeCustomer, setActiveCustomer] = useState({});
-	const [activeCustomerTrips, setActiveCustomerTrips] = useState([])
-
+	const [activeCustomerId, setActiveCustomerId] = useState(null);
+	const [bookingToAdd, setBookingToAdd] = useState(null);
+	let history = useHistory();
 	/// DEV VARIABLES ///
 
-	// const url = "https://travel-app-dg.herokuapp.com";
+	const url = "https://travel-app-dg.herokuapp.com";
 	// const devUserId = "60ae7f5a134d1a3ed0d5a818"; // set to a value valid for your dev environment
 
-	const url = "http://localhost:4500";
-	const devUserId = "60afdd9d09aef0f11875129f";   // ben's dev id.  Comment out
-
+	// const url = "http://localhost:4500";
+	const devUserId = "60ae77b0edd7b0001516475e"; // ben's dev id.  Comment out in other dev environment
 
 	/// OTHER VARIABLES ///
 	// deployed url: "https://www.notion.so/Backend-8da8f6e67fef4c3ab9cd86d1327f903e"
 
 	/// HANDLER FUNCTIONS ///
-
-	/// OTHER FUNCTIONS ///
-	const getActiveCustomer = (_id) => {
-		console.log("get active user, id:", _id);
-		fetch(url + "/customer/" + _id)
-			.then((res) => res.json())
-			.then((data) => {
-				console.log("got user data:", data.data);
-				const customer = data.data;
-				setActiveCustomer({
-					firstName: customer.name.first,
-					lastName: customer.name.last,
-					avatar: customer.avatar,
-					_id: customer._id,
-					booked_trips: [
-						...customer.booked_trips
-					],
-					username: customer.username,
-					password: customer.password
-				});
-			});
+	const handleSelectCustomer = (_id) => {
+		setActiveCustomerId(_id);
+		history.push("/users/" + _id);
 	};
 
+	const handleAddBooking = (_id) => {
+		setBookingToAdd(_id);
+		history.push("/booking");
+	};
+
+	/// OTHER FUNCTIONS ///
 
 	useEffect(() => {
-		getActiveCustomer(devUserId); // TODO change this to get active user ID from login
+		setActiveCustomerId(devUserId); // TODO change this to get active user ID from login
 	}, []);
 
 	return (
 		<div className="App">
 			<Switch>
 				<Route exact path="/">
-					<Home />
+					<Home
+						handleSelectCustomer={handleSelectCustomer}
+						activeCustomerId={activeCustomerId}
+					/>
 				</Route>
 				<Route path="/agencies/:_id">
-					<AgencyProfile />
+					<AgencyProfile activeCustomerId={activeCustomerId} />
 				</Route>
 				<Route path="/users/:_id">
-					<CustomerProfile activeCustomer={activeCustomer} />
+					<CustomerProfile
+						url={url}
+						activeCustomerId={activeCustomerId}
+						handleAddBooking={handleAddBooking}
+					/>
 				</Route>
 				<Route path="/trips">
-					<PopularTrips />
+					<PopularTrips activeCustomerId={activeCustomerId} />
 				</Route>
 				<Route path="/trips/:_id">
-					<Trip />
+					<Trip activeCustomerId={activeCustomerId} />
 				</Route>
 				<Route path="/booking">
-					<Booking />
-				</Route>
-				<Route path="/login">
-					<Login/>
+					<Booking
+						bookingToAdd={bookingToAdd}
+						activeCustomerId={activeCustomerId}
+					/>
 				</Route>
 			</Switch>
 		</div>
@@ -85,6 +78,7 @@ function App() {
 }
 
 export default App;
+
 
 // class App extends Component {
 // 	state = {
