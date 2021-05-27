de1c478c53ed5d010554009b1f5e4f511078511f
MikaSESmith <68824041+MikaSESmith@users.noreply.github.com>
2021-05-27 09:44:55 -0500

    Revert "Header2"

--- src/components/TripCard.jsx
+++ src/components/TripCard.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import {Link} from 'react-router-dom'
-// import "../dev/dummyStyle.css";
+import "../dev/dummyStyle.css";
 
 const TripCard = (props) => {
-	const altImg = "https://source.unsplash.com/QVElk1sgzY4/640x426"
 	return (
 		<Link to={`/trips/${props._id}`}>
 			<div className="trip-card">
-				<img src={props.photo ? props.photo : altImg}/>
-				<h2>{props.name}</h2>
+				<div>{props.name}</div>
 				<p>{props.desc}</p>
 			</div>
 		</Link>
