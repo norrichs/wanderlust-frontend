e684ad03617e85a33e6a8b97eb6f00d1d6715de3
Ben Norrichs <ben.norrichs@gmail.com>
2021-05-28 08:34:48 -0400

    css improvements for cards

--- src/components/AgencyCard.jsx
+++ src/components/AgencyCard.jsx
@@ -5,18 +5,29 @@ const UserTripCard = (props) => {
 	const altImg = "https://source.unsplash.com/QVElk1sgzY4/640x426";
 	return (
 		<div className="agency-card">
-			<div className="card-image-wrapper">
-				<Link to={`/trips/${props._id}`}>
-					<img src={props.logo ? props.logo : altImg} />
-				</Link>
+			<section>
+				<div>
+					<Link to={`/agency/${props._id}`}>
+						<img src={props.logo ? props.logo : altImg} />
+					</Link>
+				</div>
 
-				<div onClick={()=>{props.handleAddBooking(props._id)}} className="book-button">B</div>
-			</div>
-
-			<h2>{props.name}</h2>
-			<div>
-				<span>{props.locationName}</span>
-			</div>
+				<div>
+					<h2>{props.name}</h2>
+					<div className="star-rating">
+						<div>{"\u2605"}</div>
+						<span>4.9</span>
+					</div>
+				</div>
+			</section>
+			<section>
+				<div className="avi-small-container" >
+					<div className="user-avi-small">1</div>
+					<div className="user-avi-small">2</div>
+				</div>
+				<div className="like-count">+349</div>
+				<div className="like-count-label">Liked This</div>
+			</section>
 		</div>
 	);
 };
