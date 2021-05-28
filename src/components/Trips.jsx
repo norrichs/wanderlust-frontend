ebcc4b8c552f1a87313c54ad37fa505aa952755b
mikasesmith <clamikasmith@gmail.com>
2021-05-28 12:44:03 -0500

    pics added to cart

--- src/components/Trips.jsx
+++ src/components/Trips.jsx
@@ -6,18 +6,39 @@ const ACTIVITY = 'activity';
 export default function Products({ setCartItems, cartItems }) {
   const [trips] = useState([
     {
-      category: ACTIVITY,
-      name: 'Activity',
-      cost: 2.99,
+      category: PLACES_TO_VISIT,
+      name: 'Pico Turquino',
+      cost: 229.99,
       image:
-      "https://bn1304files.storage.live.com/y4mGc-ddj31qT21Y4-cD4qaq3n5l0pefkuNpHo73CiPbWjcrtVfLnNTHcDjHzgLcEgco5HiZasvrmt4aP0gtR2trIBBkRT6suaXhMd6jzSxntdboF_G1SHRIlOVczdo-2u_39h68UgNe4ofYCbgULr7Qj8OZGwrJTi9J-wOP3TYLyFYSkY9CIeG5RRa1NxbrERa?width=256&height=256&cropmode=none",
+      "https://source.unsplash.com/UDe7o0gtgJ4/640x959",
     },
     {
       category: PLACES_TO_VISIT,
-      name: 'Cuba1',
-      cost: 19.99,
+      name: 'El Yunque',
+      cost: 199.99,
       image:
-      "https://bn1304files.storage.live.com/y4mi4uGKC_rU450rqLVq_MbnYKQTOTOIIoXeGe8rKN6DcsqKrSOS2FxDY82jR-_vmv-_-jJMN3tnUDhJCMkH0L-srbHPux73pUUb12dZGfhJiqXdH65P563J02aKqVh7faHILLoTKeusC3oojlKNyTEhiLl-dYb48bDdhlQsWayVDeIS8EFG1id81KhY1ZJ87AV?width=256&height=256&cropmode=none",
+      "https://source.unsplash.com/OPab9u8gywo/640x960",
+    },
+    {
+      category: PLACES_TO_VISIT,
+      name: 'Jaimanitas River',
+      cost: 229.99,
+      image:
+      "https://source.unsplash.com/YvkH8R1zoQM/640x426",
+    },
+    {
+      category: ACTIVITY,
+      name: 'Sagua la Grande River',
+      cost: 229.99,
+      image:
+      "https://source.unsplash.com/y3yv_NG1kYU/640x426",
+    },
+    {
+      category: ACTIVITY,
+      name: 'Maximo River',
+      cost: 229.99,
+      image:
+      "https://source.unsplash.com/Kgj6PH1PVPM/640x425",
     },
   ]);
 
